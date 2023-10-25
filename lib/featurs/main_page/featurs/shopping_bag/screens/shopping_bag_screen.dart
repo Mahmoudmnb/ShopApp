6dc94d13325cd2d7d03f74824fc51f519c9e910b
Mahmoud Bannan <Mahmoudmnb2000.2004@gmail.com>
2023-10-24 22:32:09 -0400

    mnb

--- lib/featurs/main_page/featurs/shopping_bag/screens/shopping_bag_screen.dart
+++ lib/featurs/main_page/featurs/shopping_bag/screens/shopping_bag_screen.dart
@@ -29,7 +29,7 @@ class ShoppingBagScreen extends StatelessWidget {
                   child: CustomButton(
                     title: 'Proceed to checkout',
                     onPressed: () {
-                      log('message');
+                      log('mnb');
                     },
                   ),
                 ),
