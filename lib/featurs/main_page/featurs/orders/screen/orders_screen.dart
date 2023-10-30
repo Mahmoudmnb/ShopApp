e817ab71c7b17fe17513a4e0659d7439388d279f
Mahmoud <mahmoudAboali@gmail.com>
2023-10-30 23:05:53 +0200

    mohmmad mstake

--- lib/featurs/main_page/featurs/orders/screen/orders_screen.dart
+++ lib/featurs/main_page/featurs/orders/screen/orders_screen.dart
@@ -27,6 +27,7 @@ class MyOrdersScreen extends StatelessWidget {
           numb: '1580',
           orderDate: DateTime(2023, 9, 2),
           dueDate: DateTime(2023, 9, 20)),
+          //
       CardModel(
           kingOfOrder: 'Pending',
           quantity: 2,
